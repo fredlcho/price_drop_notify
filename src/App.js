2bc58183954739e7312e5fe632ebade4e5d05c35
Frederick <fredericklcho@gmail.com>
2019-05-27 15:44:00 -0400

    increase size of item image

--- src/App.js
+++ src/App.js
@@ -1,109 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import FormControl from 'react-bootstrap/FormControl'
-import InputGroup from 'react-bootstrap/InputGroup'
 
 const request = require('request')
 
-class Welcome extends React.Component{
-  render(){
-    return(
-      <h1> Welcome, {this.props.nameaa}</h1>
-    );
-  };
-}
-function Hello(props){
-    return <h1> Hello, {props.name}</h1>;
-}
-
-class Myclock extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  render(){
-    return(
-      <div>
-      <h1> it is now {this.state.date.toLocaleTimeString()}</h1>
-      </div>
-    );
-  };
-
-}
-
-class Clock extends React.Component{
-  constructor(props){
-    super(props);
-    this.state =
-    {
-      date: new Date(),
-      haha: 'bye'
-    };
-  }
-
-  componentDidMount(){
-    this.timerID= setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-  componentWillUnmount(){
-    clearInterval(this.timerID);
-  }
-
-  tick(){
-    this.setState({
-      haha: 'hello',
-      date:new Date()
-    });
-  }
-  render(){
-    return(
-      <div>
-        <h1> it is now {this.state.haha}.</h1>
-        <h1> it is now {this.state.date.toLocaleTimeString()}.</h1>
-      </div>
-    );
-  };
-}
-// function Comment(props) {
-//   return (
-//     <div className="Comment">
-//       <UserInfo user = {props.author}/>
-//       <div className="Comment-text">
-//         {props.text}
-//       </div>
-//       <div className="Comment-date">
-//         {formatDate(props.date)}
-//       </div>
-//     </div>
-//   );
-// }
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
-
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    );
-  }
-}
 class Thething extends React.Component{
   constructor(props){
     super(props);
@@ -141,11 +40,12 @@ class Thething extends React.Component{
         var parser = new DOMParser();
         var htmlDoc = parser.parseFromString(body,'text/html');
         console.log(htmlDoc);
+        let price;
         if (htmlDoc.getElementsByClassName('price-standard pdp-space-price')[0] == null){
-            var price = htmlDoc.getElementsByClassName('price-sales pdp-space-price sale-price-only')[0].innerHTML;
+            price = htmlDoc.getElementsByClassName('price-sales pdp-space-price sale-price-only')[0].innerHTML;
             console.log(price);
         }else{
-          var price = htmlDoc.getElementsByClassName('price-standard pdp-space-price')[0].innerHTML;
+          price = htmlDoc.getElementsByClassName('price-standard pdp-space-price')[0].innerHTML;
         }
         // var price = htmlDoc.getElementsByClassName('price-standard pdp-space-price')[0].innerHTML;
         // console.log(price);
@@ -175,7 +75,7 @@ class Thething extends React.Component{
       <div className = "iteminfo">
         <div className = "picture-class">
           <h4>Your item</h4>
-          <img id ="dapicture" src = {this.state.picture}/>
+          <img id ="dapicture" alt ="enter a link above" src = {this.state.picture}/>
         </div>
       </div>
       <div className = "iteminfo">
@@ -188,9 +88,6 @@ class Thething extends React.Component{
 
     )
   }
-
-}
-class PricexItemDisplay extends React.Component{
 
 }
 function App() {
